5a8763f746978519090a8b47121cee44ef00cd52
freya0608 <freya001@yeah.net>
2017-04-17 13:13:34 +0800

    less

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,3 +1,22 @@
 /**
  * Created by freya on 2017/4/17.
  */
+$(function () {
+    $('svg[data-src]').each(function (index, svg) {
+       var src =  $(svg).data('src');
+        
+        $.ajax({
+            url:src,
+            dataType:'xml',
+            success:function (svgDocument) {
+                var $doc = $(svgDocument.documentElement);
+                $doc.attr({
+                    width:$(svg).attr('width'),
+                    height:$(svg).attr('height')
+                })
+
+                $(svg).after($doc).remove();
+            }
+        })
+    });
+});
